ac94515895150f6ad8aa3d06cba1f2a0e495d3c2
Brian Hunley <brian.hunley@gmail.com>
2020-05-03 10:24:17 -0400

    updated packages and updated code modules.

--- index.js
+++ index.js
@@ -1,24 +1,84 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
+const chalk = require('chalk'); // colorizes the output
+const clear = require('clear'); // clears the terminal screen
+const figlet = require('figlet'); // creates ASCII art from text
 
-const github = require('./lib/github');
-const repo = require('./lib/repo');
 const files = require('./lib/files');
+const repo = require('./lib/repo');
+const github = require('./lib/github');
 
-clear();
+clear(); // clear the terminal screen
+
+// create an ASCII art title and colorized it
 console.log(
-  chalk.yellow(
-    figlet.textSync('Ginit', { horizontalLayout: 'full' })
-  )
+  chalk.yellow(figlet.textSync('Ginit', { horizontalLayout: 'full' }))
 );
 
+const getGithubToken = async () => {
+  // Fetch token from config store
+  let token = github.getStoredGithubToken();
+  if (token) {
+    return token;
+  }
+
+  // No token found, use credentials to access GitHub account
+  token = await github.getPersonalAccessToken();
+
+  return token;
+};
+
+// Does a 'git' directory already exist in the curr dir?
+//  yes, show msg and exit.
+//  no, we can create a git repo in the current dir.
 if (files.directoryExists('.git')) {
-  console.log(chalk.red('Already a git repository!'));
+  console.log(chalk.red('This directory is already a git repository!'));
   process.exit();
 }
+
+const run = async () => {
+  try {
+    // Retrieve & Set Authentication Token
+    const token = await getGithubToken();
+    github.githubAuth(token);
+
+    // Create remote repository
+    const url = await repo.createRemoteRepo();
+
+    // Create .gitignore file
+    await repo.createGitignore();
+
+    // Set up local repository and push to remote
+    await repo.setupRepo(url);
+
+    console.log(chalk.green('All done!'));
+  } catch (err) {
+    if (err) {
+      switch (err.status) {
+        case 401:
+          console.log(
+            chalk.red(
+              "Couldn't log you in. Please provide correct credentials/token."
+            )
+          );
+          break;
+        case 422:
+          console.log(
+            chalk.red(
+              'There is already a remote repository or token with the same name'
+            )
+          );
+          break;
+        default:
+          console.log(chalk.red(err));
+      }
+    }
+  }
+};
+
+run();
+
+/*
 
 const getGithubToken = async () => {
   // fetch token from config store
@@ -48,7 +108,7 @@ const getGithubToken = async () => {
   //   token = await github.regenerateNewToken(accessToken.id);
   //   return token;
   // }
-}
+};
 
 const run = async () => {
   try {
@@ -69,26 +129,24 @@ const run = async () => {
     //if (done) {
     //  console.log(chalk.green('All done!'));
     // }
-
   } catch (err) {
     if (err) {
       console.log(err);
-      /*
-      switch (err.code) {
-        case 401:
-          console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
-          break;
+      // switch (err.code) {
+      //   case 401:
+      //     console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+      //     break;
 
-        case 422:
-          console.log(chalk.red('A remote repository with the same name already exists.'));
-          break;
+      //   case 422:
+      //     console.log(chalk.red('A remote repository with the same name already exists.'));
+      //     break;
 
-        default:
-          console.log(err);
-      }
-      */
+      //   default:
+      //     console.log(err);
+      // }
     }
   }
-}
+};
+*/
 
-run();
+// run();
